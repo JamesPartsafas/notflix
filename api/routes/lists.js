3f923b4a1b4b03467b83b4e26a1dc00c23d8a048
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2021-08-27 19:16:57 -0400

    Implement list API functions

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -1,25 +1,80 @@
 const express = require('express')
-const Movie = require('../models/Movie')
+const List = require('../models/List')
 const verify = require('../verifyToken')
 
 const router = express.Router()
 
-//Create
-//Will create new film
+//CREATE
+//Will create new list
 router.post('/', verify, async (req, res) => {
     if (req.user.isAdmin) {
-        const newMovie = new Movie(req.body)
+        const newList = new List(req.body)
 
         try {
-            const savedMovie = await newMovie.save()
-            res.status(201).json(savedMovie)
+            const savedList = await newList.save()
+            res.status(201).json(savedList)
         }
         catch (err) {
             res.status(500).json(err)
         }
     }
     else {
-        res.status(403).json('You are not authorized to create movies')
+        res.status(403).json('You are not authorized to create lists')
+    }
+})
+
+//DELETE
+//Will delete list
+router.delete('/:id', verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            await List.findByIdAndDelete(req.param.id)
+            res.status(201).json('The list was successfully deleted')
+        }
+        catch (err) {
+            res.status(500).json(err)
+        }
+    }
+    else {
+        res.status(403).json('You are not authorized to create lists')
+    }
+})
+
+//GET
+//Get selected list
+router.get('/', verify, async (req, res) => {
+
+    //Type query and genre query are used to filter lists. Each list document has a type and genre as a string value
+    const typeQuery = req.query.type
+    const genreQuery = req.query.genre
+    let list = []
+
+    try {
+        if (typeQuery) {
+            //Returns only for a specific genre, else returns lists from all genres
+            if (genreQuery) {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery, genre: genreQuery } }
+                ])
+            }
+            else {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery } }
+                ])
+            }
+        }
+        else {
+            list = await List.aggregate([
+                { $sample: { size: 10 } }
+            ])
+        }
+
+        res.status(200).json(list)
+    }
+    catch(err) {
+        res.status(500).json(err)
     }
 })
 
