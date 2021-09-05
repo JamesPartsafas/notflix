fd87773d883dfe28a9d99eceaaa7cb6bf48fe1f6
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2021-09-05 19:55:49 -0400

    Implement select menu in navbar for mobile views

--- notflix-app/src/components/navbar/Navbar.jsx
+++ notflix-app/src/components/navbar/Navbar.jsx
@@ -5,12 +5,35 @@ import { Link } from 'react-router-dom'
 import { logoutHandler } from '../../authContext/apiCalls'
 import { AuthContext } from '../../authContext/authContext'
 import logo from './logo.png'
+import { makeStyles } from '@material-ui/core/styles'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel'
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+}))
 
 const Navbar = ({ type }) => {
 
+    const classes = useStyles()
+
     const [isScrolled, setIsScrolled] = useState(false)
+    const [page, setPage] = useState('')
 
     const {dispatch} = useContext(AuthContext)
+
+    
+    const handleChange = (event) => {
+        setPage(event.target.value);
+    }
 
     //Detect window scrolling to determine navbar color
     window.onscroll = () => {
@@ -34,18 +57,44 @@ const Navbar = ({ type }) => {
             <div className="container">
                 <div className="leftSection">
                     <img src={logo} alt="" />
-                    <Link to='/' className="link">
-                        <span className={`navbarMainLinks ${type == null && 'selected'}`}>Home</span>
-                    </Link>
-                    <Link to='/series' className="link">
-                        <span className={`navbarMainLinks ${type === 'series' && 'selected'}`}>TV Shows</span>
-                    </Link>
-                    <Link to='/movies' className="link">
-                        <span className={`navbarMainLinks ${type === 'movie' && 'selected'}`}>Movies</span>
-                    </Link>
-                    <Link to='/mylist' className="link">
-                        <span className={`navbarMainLinks ${type === 'list' && 'selected'}`}>My List</span>
-                    </Link>
+                    <div className="navLinks">
+                        <Link to='/' className="link">
+                            <span className={`navbarMainLinks ${type == null && 'selected'}`}>Home</span>
+                        </Link>
+                        <Link to='/series' className="link">
+                            <span className={`navbarMainLinks ${type === 'series' && 'selected'}`}>TV Shows</span>
+                        </Link>
+                        <Link to='/movies' className="link">
+                            <span className={`navbarMainLinks ${type === 'movie' && 'selected'}`}>Movies</span>
+                        </Link>
+                        <Link to='/mylist' className="link">
+                            <span className={`navbarMainLinks ${type === 'list' && 'selected'}`}>My List</span>
+                        </Link>
+                    </div>
+
+                    <FormControl variant="" className={`${classes.formControl} selectForm`}>
+                        <InputLabel className="selectLabel" id="selectedPage">Browse</InputLabel>
+                        <Select
+                            labelId="selectedPage"
+                            id="selectedPage"
+                            value={page}
+                            onChange={handleChange}
+                            className="selectShown"
+                        >
+                            <Link to='/' className="link">
+                                <MenuItem className="menuItem" value="home">Home</MenuItem>
+                            </Link>
+                            <Link to='/series' className="link">
+                                <MenuItem className="menuItem" value='series'>TV Shows</MenuItem>
+                            </Link>
+                            <Link to='/movies' className="link">
+                                <MenuItem className="menuItem" value='movie'>Movies</MenuItem>
+                            </Link>
+                            <Link to='/mylist' className="link">
+                                <MenuItem className="menuItem" value='list'>My List</MenuItem>
+                            </Link>
+                        </Select>
+                    </FormControl>
                 </div>
                 <div className="rightSection">
                     <Search className="icon navbarMainLinks" />
